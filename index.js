40abf52bc7c4509eb2f9690ce271c565b43876ab
sheygs13 <segun.ekoh@gmail.com>
2020-03-18 15:06:04 +0100

    setup mongoDB database & route handlers

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 
+
+mongoose.connect('mongodb://localhost/movie-booking-app')
+        .then(() => console.log('Connected to MongoDB...'))
+        .catch(error => console.log('Could not connect to MongoDB',error));
+
 app.use(express.json());
 app.use('/api/v1/genres', genres);
+app.use('/api/v1/customers', customers);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
